b32fc85882bb7c48109c58f6ab97c7ae45794d02
kuiguo1991 <kuiguo1991@gmail.com>
2019-01-21 18:01:40 +0800

    提交2019/01/21#01

--- routers/Weather.js
+++ routers/Weather.js
@@ -2,21 +2,21 @@
  * Created by admin on 2018/12/28.
  *
  * 测试调用Java写的webservice模块
- * 
+ *
  * 中国天气接口
  */
 
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 var soap = require("soap");
 
 //中国天气
 const url = "http://ws.webxml.com.cn/WebServices/WeatherWS.asmx?wsdl";
 
-
 router.get("/getRegionProvince", function(req, res) {
   soap.createClient(url, "utf8", function(err, client) {
     client.getRegionProvince(function(err, result) {
+      // console.log(result);
       var d = result.getRegionProvinceResult.string;
       var dd = [];
       for (var i = 0; i < d.length; i++) {
@@ -38,4 +38,17 @@ router.get("/getRegionProvince", function(req, res) {
   });
 });
 
-module.exports = router
+
+router.post("/getWeather", function(req, res) {
+  const param = req.body || req.query || req.params;
+  console.log(param);
+  soap.createClient(url, "utf8", function(err, client) {
+    client.getWeather(param, function(err, result) {
+      // console.log(result);
+      var dd = result.getWeatherResult.string;
+      var data = JSON.stringify({ code: 0, msg: "请求成功", result: dd });
+      res.send(data);
+    });
+  });
+});
+module.exports = router;
