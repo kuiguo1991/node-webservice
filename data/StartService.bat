8282e973905d4bf3f293227100b5488ea2b22513
yuguo1991 <kuiguo1991@gmail.com>
2019-02-25 22:25:58 +0800

    提交2019/02/25#01

--- data/StartService.bat
+++ data/StartService.bat
@@ -1,2 +1,2 @@
 @echo off
-pm2 start D:\github\node-login\app.js D:\github\node-webservice\app.js D:\github\ReactTraining\server\app.js D:\github\ygVueTraining\server\app.js
+pm2 start D:\github\node-login\app.js D:\github\node-webservice\app.js D:\github\Ant-Design-Node\server\app.js D:\github\element-ui-node\server\app.js
