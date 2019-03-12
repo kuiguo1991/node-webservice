23f94109a9a28ed690b4a42d95290e11568b66cf
kuiguo1991 <kuiguo1991@gmail.com>
2019-03-12 10:35:10 +0800

    提交2019/03/12#01

---
+++ apidoc/api_data.js
@@ -0,0 +1,35 @@
+define({ "api": [
+  {
+    "type": "get",
+    "url": "/wsdl/ChinaTV/getAreaDataSet",
+    "title": "区域数据",
+    "description": "<p>获取区域数据</p>",
+    "name": "getAreaDataSet",
+    "group": "ChinaTV",
+    "version": "1.1.0",
+    "filename": "routers/index.js",
+    "groupTitle": "ChinaTV"
+  },
+  {
+    "type": "get",
+    "url": "/wsdl/Weather/getRegionProvince",
+    "title": "天气获取",
+    "description": "<p>获取天气</p>",
+    "name": "getRegionProvince",
+    "group": "Weather",
+    "version": "1.0.0",
+    "filename": "routers/index.js",
+    "groupTitle": "Weather"
+  },
+  {
+    "type": "get",
+    "url": "/wsdl/Weather/getWeather",
+    "title": "天气获取",
+    "description": "<p>获取天气</p>",
+    "name": "getWeather",
+    "group": "Weather",
+    "version": "1.0.0",
+    "filename": "routers/index.js",
+    "groupTitle": "Weather"
+  }
+] });
--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,7 @@
  *
  * 测试调用Java写的webservice模块
  *
- * 
+ *
  */
 
 const express = require("express");
@@ -11,9 +11,34 @@ const router = express.Router();
 const ChinaTV = require("../constrollers/ChinaTV");
 const Weather = require("../constrollers/Weather");
 
+
+/**
+* @api {get} /wsdl/Weather/getRegionProvince 天气获取
+* @apiDescription 获取天气
+* @apiName getRegionProvince
+* @apiGroup Weather
+* @apiVersion 1.0.0
+*/
+
+
 router.get("/Weather/getRegionProvince", Weather.getRegionProvince);
+
+/**
+* @api {get} /wsdl/Weather/getWeather 天气获取
+* @apiDescription 获取天气
+* @apiName getWeather
+* @apiGroup Weather
+* @apiVersion 1.0.0
+*/
 router.post("/Weather/getWeather", Weather.getWeather);
 
+/**
+* @api {get} /wsdl/ChinaTV/getAreaDataSet 区域数据
+* @apiDescription 获取区域数据
+* @apiName getAreaDataSet
+* @apiGroup ChinaTV
+* @apiVersion 1.1.0
+*/
 router.get("/ChinaTV/getAreaDataSet", ChinaTV.getAreaDataSet);
 
 module.exports = router;
